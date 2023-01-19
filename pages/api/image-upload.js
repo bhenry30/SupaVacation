18cc36bd1f58477ca84bf2848fb6f5fb52ed91c3
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2023-01-19 16:03:46 -0600

    adds logic to async handler function for POST method in image-upload.js

--- pages/api/image-upload.js
+++ pages/api/image-upload.js
@@ -1,14 +1,57 @@
 import { createClient } from '@supabase/supabase-js';
+import { nanoid } from 'nanoid'
+import { decode } from 'base64-arraybuffer';
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_KEY,
 )
 
+export const config = {
+    api: {
+      bodyParser: {
+        sizeLimit: '10mb',
+      },
+    },
+  };
+
 export default async function handler(req, res) {
     // Upload image to Supabase
     if (req.method === 'POST') {
-        // TODO
+        let { image } = req.body;
+
+        if (!image) {
+            return res.status(500).json({ message: 'Image data not valid' })
+        }
+        try{
+            const contentType = image.match(/data:(.*);base64/)?.[1];
+            const base64FileData = image.split('base64')?.[1];
+
+            if (!contentType || !base64FileData) {
+                return res.status(500).json({ message: 'Image data not valid' });
+            }
+
+            const fileName = nanoid();
+            const ext = contentType.split('/')[1];
+            const path = `${fileName}.$[ext]`
+
+            const { data, error: uploadError } = await supabase.storage
+            .from(process.env.SUPABASE_BUCKET)
+            .upload(path, decode(base64FileData), {
+                contentType,
+                upsert: true,
+            });
+
+            if (uploadError) {
+                throw new Error('Unable to upload image to storage');
+            }
+
+            const url = `${process.env.SUPABASE_URL.replace('.co', '.in')}/storage/v1/object/public/${data.Key}`;
+
+            return res.status(200).json({ url });
+        } catch (e) {
+            res.status(500).json({ message: 'Something went wrong'})
+        }
     }
     //HTTP method not supported
     else {
@@ -19,15 +62,3 @@ export default async function handler(req, res) {
     }    
 }
 
-let { image } = req.body;
-
-if (!image) {
-    return res.status(500).json({ message: 'Image data not valid' })
-}
-
-const contentType = image.match(/data:(.*);base64/)?.[1];
-const base64FileData = image.split('base64')?.[1];
-
-if (!contentType || !base64FileData) {
-    return res.status(500).json({ message: 'Image data not valid' });
-  }
